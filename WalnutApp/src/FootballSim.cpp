dba437e007570f30b55865d4224f034e0b626f85
Stollaz <stollerymatthew@gmail.com>
2022-06-24 01:00:59 +0100

    fixed include so it builds now

--- WalnutApp/src/FootballSim.cpp
+++ WalnutApp/src/FootballSim.cpp
@@ -8,7 +8,29 @@
 #include <queue>
 #include <wtypes.h>
 
-#include "include.h"
+#define FMT_HEADER_ONLY
+#include "fmt/core.h"
+#include "fmt/format.h"
+#include "fmt/xchar.h"
+#include "fmt/color.h"
+
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+#include "glm/mat3x3.hpp"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <fstream>
+#include <locale>
+#include <time.h>
+#include <locale.h>
+#include <io.h>
+#include <fcntl.h>
+#include <Windows.h>
+#include <windows.h>
+#include <chrono>
 
 using namespace Walnut;
 
